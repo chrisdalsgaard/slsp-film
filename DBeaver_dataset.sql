8dabea1f89ae6e5b70ea6725811e7b77d456a7f0
Lisa Bjerge Krogholt <lisa.bjergekrogholt@dxc.com>
2023-02-17 15:36:43 +0100

    Ny select med inner join

--- DBeaver_dataset.sql
+++ DBeaver_dataset.sql
@@ -1,5 +1,10 @@
 
-/* Select Artister som starter med B*/
+/* Select Artister som starter med B */
 SELECT ArtistID, Name
 FROM Album
 WHERE Name = 'B%';
+
+/* List alle albums per artist */
+SELECT ART.Name, ART.ArtistId, ALB.Title
+FROM Artist as ART
+INNER JOIN Album as ALB ON ART.ArtistId = ALB.ArtistIde;
